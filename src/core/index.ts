dca8be7e0d046c6c04d5142074b938cd900c678b
AlexisPiquet <alexis.piquet.37@gmail.com>
2023-03-18 02:33:26 +0100

    [patch-release]

--- src/core/index.ts
+++ src/core/index.ts
@@ -27,29 +27,29 @@ export default class Core<T> extends Repository<ObjectLiteral>{
   }
 
   public async get (request: Request): Promise<ObjectLiteral[]> {
-    debug('GET [ %s ] request=%o', this.model, request)
+    debug('GET [ %s ] request=%o', `${this.model}`, request)
     return this.store.find()
   }
 
   public async read (request: Request): Promise<ObjectLiteral | null | undefined> {
     const filters = request?.query?.filters
     if (!filters) return undefined
-    debug('READ [ %s ] REQUEST=%o FILTERS=%o FILTERS=%o', this.model, request, filters)
+    debug('READ [ %s ] REQUEST=%o FILTERS=%o FILTERS=%o', `${this.model}`, request, filters)
     return this.store.findBy(filters)
   }
 
   public async post (request: Request): Promise<DeepPartial<ObjectLiteral>> {
-    debug('POST [ %s ] request=%o', this.model, request)
+    debug('POST [ %s ] request=%o', `${this.model}`, request)
     return this.store.save(request.payload)
   }
 
   public async put (request: Request): Promise<DeepPartial<ObjectLiteral>> {
-    debug('PUT [ %s ] request=%o', this.model, request)
+    debug('PUT [ %s ] request=%o', `${this.model}`, request)
     return this.store.save(request.payload)
   }
 
   public async deleteOne (request: Request): Promise<ObjectLiteral> {
-    debug('DELETE [ %s ] request=%o', this.model, request)
+    debug('DELETE [ %s ] request=%o', `${this.model}`, request)
     return this.store.remove({ id: request.payload.id })
   }
 }
